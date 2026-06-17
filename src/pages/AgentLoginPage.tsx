defc2730cc28d9ffc9f7648f576ecb67552012c6
ypniel <ypniel@holmes.edu.au>
2026-06-17 11:30:53 +1000

    Update AgentLoginPage.tsx

--- src/pages/AgentLoginPage.tsx
+++ src/pages/AgentLoginPage.tsx
@@ -98,7 +98,7 @@ export default function AgentLoginPage() {
       <div className="absolute top-6 left-8 z-10">
         <div className="flex items-center gap-2.5">
           <img
-            src="https://holmes.edu.au/templates/images/Logo-base-banner.png"
+            src="https://39917994.fs1.hubspotusercontent-na1.net/hubfs/39917994/EDM%20Headers%20(1).png"
             alt="Holmes"
             className="h-8 w-auto"
             onError={(e) => { e.currentTarget.style.display = "none" }}
