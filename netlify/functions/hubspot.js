f40b01a937d0788823fd4a05e066cca434b3b1a2
ypniel <ypniel@holmes.edu.au>
2026-06-20 15:47:31 +1000

    Update hubspot.js

--- netlify/functions/hubspot.js
+++ netlify/functions/hubspot.js
@@ -1,5 +1,5 @@
 const https = require("https")
-const TOKEN = process.env.HUBSPOT_TOKEN || process.env.VITE_HUBSPOT_TOKEN
+const TOKEN = process.env.HUBSPOT_TOKEN
 const PIPELINE_ID = process.env.VITE_PIPELINE_ID || "789344406"
 function makeRequest(options, body) {
   return new Promise((resolve, reject) => {
